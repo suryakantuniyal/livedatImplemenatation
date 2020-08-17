653d96d3a934780c5b0672b2bca420a129380598
suryakant.uniyal <suryakantuniyal7@gmail.com>
2020-08-17 16:17:43 +0530

    my commit

--- TEstingUpdated/src/androidTest/java/com/example/testingexample/ExampleInstrumentedTest.java
+++ TEstingUpdated/src/androidTest/java/com/example/testingexample/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.testingexample;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -22,5 +23,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.testingexample", appContext.getPackageName());
+        Log.d("hello","hello this is for testing");
     }
 }
